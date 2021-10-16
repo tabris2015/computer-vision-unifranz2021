e6472ce240d593e94abf3ada5402b93664c2f198
Jose Laruta <eduardo.laruta@gmail.com>
2021-10-16 10:54:10 -0400

    update

--- tensorflow/image_classification_webcam.py
+++ tensorflow/image_classification_webcam.py
@@ -1,27 +1,32 @@
 import numpy as np
+from time import time
 import cv2
 import tensorflow as tf
 import keras
-from keras.applications.mobilenet import MobileNet, preprocess_input, decode_predictions
+from keras.applications.efficientnet import EfficientNetB7, preprocess_input, decode_predictions
 from keras.preprocessing import image
 
 
 class Classifier:
     def __init__(self, img_size) -> None:
         self.img_size = img_size
-        self.model = MobileNet(weights="imagenet")
+        self.model = EfficientNetB7(weights="imagenet")
         self.font = cv2.FONT_HERSHEY_SIMPLEX
         
     def predict(self, img, top=3):
 
         img = cv2.resize(img, self.img_size)
+        # dims (224, 224, 3)
 
         # x = image.img_to_array(img)
+        # dims (1, 224, 224, 3)
         x = np.expand_dims(img, axis=0)
         x = preprocess_input(x)
 
+        start_time = time()
         preds = self.model.predict(x)
         decoded = decode_predictions(preds, top=top)[0]
+        print(f"Tiempo: {(time() - start_time)*1000:.1f}[ms]")
         return {label:conf for _, label, conf in decoded}
 
     def predict_and_display(self, img, top=3):
@@ -40,7 +45,7 @@ class Classifier:
 
 cap = cv2.VideoCapture(1)
 
-clf = Classifier(img_size=(224, 224))
+clf = Classifier(img_size=(600, 600))
 # verificar conexion
 if not cap.isOpened():
     print("No se puede abrir webcam")
@@ -49,12 +54,9 @@ while True:
     # lectura de un frame
     ret, frame = cap.read()
     # operaciones con el frame
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # out = procesar_frame(frame)
     out = clf.predict_and_display(frame)
     # mostrar imagenes en una ventana
-    # cv2.imshow("input", frame)
-    cv2.imshow("gray", out)
+    cv2.imshow("out", out)
 
     # detectar una tecla
     c = cv2.waitKey(1)
